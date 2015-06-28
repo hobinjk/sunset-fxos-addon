44429d4c344a336743e4797a50271326df274b4a
April King <april@mozilla.com>
2015-06-28 17:18:53 -0500

    Another huge mass of changes

--- js/sunset.js
+++ js/sunset.js
@@ -1,27 +1,106 @@
+function init() {
+  var DEFAULT_SETTINGS = {
+    'sunset-max-alpha': 'screen.sunset.max-alpha',
+    'sunset-manual-sunrise': 'screen.sunset.manual-sunrise',
+    'sunset-manual-sunset': 'screen.sunset.manual-sunset'
+  };
+
+  for (var setting in DEFAULT_SETTINGS) {
+    getSetSettings(setting, DEFAULT_SETTINGS[setting]);
+  }
+
+}
+
+function getSetSettings (domNodeId, systemPref) {
+  'use strict';
+
+  domNodeId = String(domNodeId);
+  systemPref = String(systemPref);
+  var event;
+
+  var node = document.getElementById(String(domNodeId));
+  if (node.type === 'time') { event = 'input'; }
+  else { event = 'change'; }
+
+  var req = navigator.mozSettings.createLock().get(systemPref);
+  req.onsuccess = function() {
+    if (systemPref in req.result) {
+      console.log('setting page pref to: ' + String(req.result[systemPref]));
+      node.value = req.result[systemPref];
+    }
+  };
+
+  node.addEventListener(event, function() {
+    console.log('changed');
+    console.log('updating system pref: ' + systemPref + ' to ' + String(node.value));
+    var obj = {};
+    obj[systemPref] = node.value;
+    console.log(obj);
+    navigator.mozSettings.createLock().set(obj);
+  });
+}
+
 window.addEventListener('DOMContentLoaded', function () {
   'use strict';
 
-  var sunsetEnabled = document.getElementById('sunset-enabled');
+  init();
 
-  /* Set the system setting */
-  // lock = navigator.mozSettings.createLock();
-  // lock.set({
-  //   'screen.sunset.enabled': true
-  // });
+  // var geolocationEnabled = document.getElementById('geolocation-enabled');
+  // var geolocationLocation = document.getElementById('geolocation-location');
+  // var sunsetMaxAlpha = document.getElementById('sunset-max-alpha');
+  // var sunsetManualTimeSunrise = document.getElementById('sunset-manual-time-sunrise');
+  // var sunsetManualTimeSunset = document.getElementById('sunset-manual-time-sunset');
 
-  sunsetEnabled.onchange = function () {
-    var lock;
+  // var req;
 
-    if (sunsetEnabled.checked === true) {
-      lock = navigator.mozSettings.createLock();
-      lock.set({
-        'screen.sunset.enabled': true
-      });
-    } else {
-      lock = navigator.mozSettings.createLock();
-      lock.set({
-        'screen.sunset.enabled': false
-      });
-    }
-  };
+  /* The settings API is a horrible thing to work with */
+  // req = navigator.mozSettings.createLock().get('screen.sunset.max-alpha');
+  // req.onsuccess = function() {
+  //   if ('screen.sunset.max-alpha' in req.result) {
+  //     sunsetMaxAlpha.value = req.result['screen.sunset.max-alpha'];
+  //   }
+  // };
+
+  // req = navigator.mozSettings.createLock().lock.get('screen.sunset.manual-sunrise');
+  // req.onsuccess = function() {
+  //   if ('screen.sunset.manual-sunrise' in req.result) {
+  //     sunsetManualTimeSunrise.value = req.result['screen.sunset.manual-sunrise'];
+  //   }
+  // };
+
+  // req = navigator.mozSettings.createLock().lock.get('screen.sunset.manual-sunset');
+  // req.onsuccess = function() {
+  //   if ('screen.sunset.manual-sunset' in req.result) {
+  //     sunsetManualTimeSunset.value = req.result['screen.sunset.manual-sunset'];
+  //   }
+  // };
+
+
+  // sunsetMaxAlpha.onchange = function () {
+  //     var lock = navigator.mozSettings.createLock();
+  //     lock.set({
+  //     'screen.sunset.max-alpha': sunsetMaxAlpha.value
+  //   });
+  // };
+
+  /* Set the global system setting to enable or disable the filter */
+  // sunsetEnabled.onchange = function () {
+  //   var lock = navigator.mozSettings.createLock();
+  //   lock.set({
+  //     'screen.sunset.enabled': sunsetEnabled.checked
+  //   });
+  // };
+
+  // /* If geolocation is disabled, enable manual geolocation setting */
+  // geolocationEnabled.onchange = function () {
+  //   geolocationLocation.disabled = geolocationEnabled.checked;
+  // };
+
+  //  Commit the setting for location when it is entered by a user
+  // geolocationLocation.onchange = function () {
+  //   var lock = navigator.mozSettings.createLock();
+  //   lock.set({
+  //     'screen.sunset.location': geolocationLocation.value
+  //   });
+  // };
 });
--- js/system.js
+++ js/system.js
@@ -1,7 +1,14 @@
 /*eslint-env browser */
 
 var Sunset = (function() {
-  var GLOBAL_MAX_ALPHA = .25;
+  'use strict';
+
+  var DEFAULT_SETTINGS = {
+    'screen.sunset.geolocate': true,
+    'screen.sunset.max-alpha': 25,
+    'screen.sunset.manual-sunrise': '06:00:00',
+    'screen.sunset.manual-sunset': '18:00:00'
+  };
 
   function geolocate() {
     // TODO
@@ -11,19 +18,55 @@ var Sunset = (function() {
     // TODO
   }
 
-  function initialize() {
-    'use strict';
+  function setUp() {
+    console.log('entering setUp()');
 
-    var filter, lock;
+    for (var setting in DEFAULT_SETTINGS) {
+      setDefaultValue(setting, DEFAULT_SETTINGS[setting]);
+    }
+  }
 
-    /* Reset the enablement setting to true whenever the add-on is toggled in Settings -> Add-Ons */
-    lock = navigator.mozSettings.createLock();
-    lock.set({
-      'screen.sunset.enabled': true
-    });
+  function setDefaultValue(setting, value) {
+    setting = String(setting);
+    console.log('entering setDefaultValue() to set ' + setting + 'to ' + String(value));
+
+    var req = navigator.mozSettings.createLock().get(setting);
+    req.onsuccess = function () {
+      if (!(setting in req.result)) {
+        console.log('setting ' + setting + ' to ' + String(value));
+        var obj = {};
+        obj[setting] = value;
+        navigator.mozSettings.createLock().set(obj);
+      } else {
+        console.log('setting ' + setting + ' already found as: ' + req.result[setting]);
+      }
+    };
+
+  }
+
+  function init() {
+    console.log('entering init()');
+    /* Set the listeners, launching Sunset whenever it gets enabled */
+    navigator.mozApps.mgmt.onuninstall = uninstall;
+    navigator.mozApps.mgmt.onenabledstatechange = function(event) {
+      toggle(event.application.enabled);
+    };
+
+    /* Set the listener that checks for changes from Sunset.app to screen.sunset.enabled */
+    // navigator.mozSettings.addObserver('screen.sunset.enabled', function (event) { toggle(event.settingValue); });
+    navigator.mozSettings.addObserver('screen.sunset.max-alpha', function (event) { toggle(true); });
+
+    /* Turn the darned thing on */
+    toggle(true);
+  }
+
+  function inject() {
+    var filter;
+    console.log('entering inject()');
 
     /* Don't allow the filter to be set infinite times; we don't love sunsets *that* much */
     if (document.contains(document.getElementById('sunset'))) {
+      console.log('exiting inject() - sunset already exists');
       return;
     }
 
@@ -32,7 +75,6 @@ var Sunset = (function() {
     filter.id = 'sunset';
 
     /* set all the styles for the filter */
-    filter.style.backgroundColor = 'rgba(255, 255, 0, ' + String(GLOBAL_MAX_ALPHA) + ')';
     filter.style.height = '100%';
     filter.style.left = 0;
     filter.style.pointerEvents = 'none';
@@ -44,59 +86,64 @@ var Sunset = (function() {
     /* append the screen filter to the system app */
     document.body.appendChild(filter);
 
-    /* Set the listeners */
-    navigator.mozApps.mgmt.onuninstall = uninstall;
-    navigator.mozApps.mgmt.onenabledstatechange = function(event) { toggle(event.application.enabled); };
-
-    navigator.mozSettings.addObserver('screen.sunset.enabled', function (event) { toggle(event.settingValue); });
+    console.log('successfully injected sunset into system');
   }
 
   /* Set the alpha transparency of the screen filter */
   function setAlpha(alpha) {
-    'use strict';
+    console.log('entering setAlpha() with alpha of ' + String(alpha / 100));
 
     var filter = document.getElementById('sunset');
-    filter.style.backgroundColor = 'rgba(255, 255, 0, ' + String(alpha) + ')';
+    filter.style.backgroundColor = 'rgba(255, 255, 0, ' + String(alpha / 100) + ')';
   }
 
   /* Toggle the screen filter on and off */
   function toggle(onoff) {
-    'use strict';
-
-    // var lock = navigator.mozSettings.createLock();
+    console.log('entering toggle()');
 
     if (onoff === true) {
-      setAlpha(GLOBAL_MAX_ALPHA);
+      inject();
+
+      var lock = navigator.mozSettings.createLock();
+      var req = lock.get('screen.sunset.max-alpha');
+      req.onsuccess = function () {
+        setAlpha(req.result['screen.sunset.max-alpha']);
+      };
     } else {
-      setAlpha(0); // this could probably eventually be changed to uninstall it, for performance reasons
+      uninstall(); // I could just set the alpha to zero, but presumably removing the div will increase performance
     }
+    console.log('exiting toggle()');
   }
 
   function uninstall() {
-    'use strict';
-
+    console.log('entering uninstall()');
     var filter = document.getElementById('sunset');
     filter.parentNode.removeChild(filter);
+    console.log('exiting uninstall()');
   }
 
+
   return {
-    initialize: initialize,
+    init: init,
+    inject: inject,
     setAlpha: setAlpha,
+    setUp: setUp,
     uninstall: uninstall
   };
 }());
 
 /* Attempt to initialize the application */
 if (document.documentElement) {
-  Sunset.initialize();
+  console.log('enabling sunset inside system on being enabled');
+  Sunset.setUp();
+  Sunset.inject();
+  Sunset.init();
 } else {
-  window.addEventListener('DOMContentLoaded', Sunset.initialize);
-}
+  window.addEventListener('DOMContentLoaded', function () {
+    'use strict';
+    console.log('enabling sunset inside system on startup');
 
-// navigator.mozApps.mgmt.getAll().then(function(apps) {
-//   apps.forEach(function(app) {
-//     if (app.manifest.name === 'Sunset') {
-//       app.launch();
-//     }
-//   });
-// });
+    Sunset.inject();
+    Sunset.init();
+  });
+}
