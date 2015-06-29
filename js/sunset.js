676df96cd3080e7684a93c6041f791b338089ab8
April King <april@mozilla.com>
2015-06-29 13:03:04 -0500

    Much documentation, code cleanup, new iconography

--- js/sunset.js
+++ js/sunset.js
@@ -1,106 +1,53 @@
+/*eslint-env browser */
+
+
+/* Sets all the initial values for the inputs on the page, and then binds events to update the system preferences
+   when they're changed */
+function getSetSettings (domNodeId, systemPref) {
+  'use strict';
+
+  var event;
+  domNodeId = String(domNodeId);
+  systemPref = String(systemPref);
+
+  /* find the node on the page, determine the appropriate event type */
+  var node = document.getElementById(String(domNodeId));
+  if (node.type === 'time') { event = 'input'; } // time goes a bit crazy with onchange
+  else { event = 'change'; }
+
+  /* Get all of the system settings, and apply the values to the inputs */
+  var req = navigator.mozSettings.createLock().get(systemPref);
+  req.onsuccess = function() {
+    if (systemPref in req.result) {
+      node.value = req.result[systemPref];
+    }
+  };
+
+  /* Whenever an input is changed, update the system setting */
+  node.addEventListener(event, function() {
+    var obj = {};
+    obj[systemPref] = node.value;
+    navigator.mozSettings.createLock().set(obj);
+  });
+}
+
+/* Use the system settings to set the values on all of the inputs on the page */
 function init() {
-  var DEFAULT_SETTINGS = {
+  var ID_SETTING_MAPPING = {
     'sunset-max-alpha': 'screen.sunset.max-alpha',
     'sunset-manual-sunrise': 'screen.sunset.manual-sunrise',
     'sunset-manual-sunset': 'screen.sunset.manual-sunset'
   };
 
-  for (var setting in DEFAULT_SETTINGS) {
-    getSetSettings(setting, DEFAULT_SETTINGS[setting]);
+  for (var setting in ID_SETTING_MAPPING) {
+    getSetSettings(setting, ID_SETTING_MAPPING[setting]);
   }
 
 }
 
-function getSetSettings (domNodeId, systemPref) {
-  'use strict';
-
-  domNodeId = String(domNodeId);
-  systemPref = String(systemPref);
-  var event;
-
-  var node = document.getElementById(String(domNodeId));
-  if (node.type === 'time') { event = 'input'; }
-  else { event = 'change'; }
-
-  var req = navigator.mozSettings.createLock().get(systemPref);
-  req.onsuccess = function() {
-    if (systemPref in req.result) {
-      console.log('setting page pref to: ' + String(req.result[systemPref]));
-      node.value = req.result[systemPref];
-    }
-  };
-
-  node.addEventListener(event, function() {
-    console.log('changed');
-    console.log('updating system pref: ' + systemPref + ' to ' + String(node.value));
-    var obj = {};
-    obj[systemPref] = node.value;
-    console.log(obj);
-    navigator.mozSettings.createLock().set(obj);
-  });
-}
-
+/* Start 'er up! */
 window.addEventListener('DOMContentLoaded', function () {
   'use strict';
 
   init();
-
-  // var geolocationEnabled = document.getElementById('geolocation-enabled');
-  // var geolocationLocation = document.getElementById('geolocation-location');
-  // var sunsetMaxAlpha = document.getElementById('sunset-max-alpha');
-  // var sunsetManualTimeSunrise = document.getElementById('sunset-manual-time-sunrise');
-  // var sunsetManualTimeSunset = document.getElementById('sunset-manual-time-sunset');
-
-  // var req;
-
-  /* The settings API is a horrible thing to work with */
-  // req = navigator.mozSettings.createLock().get('screen.sunset.max-alpha');
-  // req.onsuccess = function() {
-  //   if ('screen.sunset.max-alpha' in req.result) {
-  //     sunsetMaxAlpha.value = req.result['screen.sunset.max-alpha'];
-  //   }
-  // };
-
-  // req = navigator.mozSettings.createLock().lock.get('screen.sunset.manual-sunrise');
-  // req.onsuccess = function() {
-  //   if ('screen.sunset.manual-sunrise' in req.result) {
-  //     sunsetManualTimeSunrise.value = req.result['screen.sunset.manual-sunrise'];
-  //   }
-  // };
-
-  // req = navigator.mozSettings.createLock().lock.get('screen.sunset.manual-sunset');
-  // req.onsuccess = function() {
-  //   if ('screen.sunset.manual-sunset' in req.result) {
-  //     sunsetManualTimeSunset.value = req.result['screen.sunset.manual-sunset'];
-  //   }
-  // };
-
-
-  // sunsetMaxAlpha.onchange = function () {
-  //     var lock = navigator.mozSettings.createLock();
-  //     lock.set({
-  //     'screen.sunset.max-alpha': sunsetMaxAlpha.value
-  //   });
-  // };
-
-  /* Set the global system setting to enable or disable the filter */
-  // sunsetEnabled.onchange = function () {
-  //   var lock = navigator.mozSettings.createLock();
-  //   lock.set({
-  //     'screen.sunset.enabled': sunsetEnabled.checked
-  //   });
-  // };
-
-  // /* If geolocation is disabled, enable manual geolocation setting */
-  // geolocationEnabled.onchange = function () {
-  //   geolocationLocation.disabled = geolocationEnabled.checked;
-  // };
-
-  //  Commit the setting for location when it is entered by a user
-  // geolocationLocation.onchange = function () {
-  //   var lock = navigator.mozSettings.createLock();
-  //   lock.set({
-  //     'screen.sunset.location': geolocationLocation.value
-  //   });
-  // };
 });
--- js/system.js
+++ js/system.js
@@ -1,29 +1,31 @@
 /*eslint-env browser */
 
+/* Try not to pollute the global namespace too much */
 var Sunset = (function() {
   'use strict';
 
+  /* a carefully chosen set of times, based on what my gut and eyeballs told me */
   var DEFAULT_SETTINGS = {
     'screen.sunset.geolocate': true,
-    'screen.sunset.max-alpha': 25,
+    'screen.sunset.max-alpha': 17, // your foxfood may vary
     'screen.sunset.manual-sunrise': '07:00',
     'screen.sunset.manual-sunset': '20:00'
   };
 
   /* Replicate the Sunset settings into SunsetSettings, so we don't have to constantly make async
-     calls to the painful Settings API */
+     calls to the painful Settings API.  Once an update is made, call toggle so that the screen
+     properly reflects the change */
   var SunsetSettings = {};
-
   function syncMozSettings(event) {
-    console.log('entering syncMozSettings()');
     SunsetSettings[String(event.settingName)] = event.settingValue;
     toggle(true);
-    console.log(SunsetSettings);
   }
 
+  /* Whenever the application is initialized, we need to do some setup to:
+    1) Replicate the DEFAULT_SETTINGS to Global Settings, if they don't already exist
+    2) Push the proper setting (either default or global into mozSyncSettings())
+    3) Set observers that call back to syncMozSettings whenever they're changed */
   function setUp() {
-    console.log('entering setUp()');
-
     for (var setting in DEFAULT_SETTINGS) {
       setUpGetSetMonitorValues(setting, DEFAULT_SETTINGS[setting]);
     }
@@ -31,59 +33,60 @@ var Sunset = (function() {
 
   function setUpGetSetMonitorValues(setting, value) {
     setting = String(setting);
-    console.log('entering setUpGetSetMonitorValues() to set ' + setting + 'to ' + String(value));
 
-    var syncevent = {};
-    syncevent.settingName = setting;
+    /* Create a fake event like what createLock().get() returns that we will use when calling syncMozSettings */
+    var event = {};
+    event.settingName = setting;
 
-    var req = navigator.mozSettings.createLock().get(setting);
+    var req = navigator.mozSettings.createLock().get(setting); // the Settings API makes this unnecessarily difficult
     req.onsuccess = function () {
-      if (!(setting in req.result)) {
-        console.log('setting ' + setting + ' to ' + String(value));
+      if (!(setting in req.result)) { // global pref doesn't exist yet
         var obj = {};
         obj[setting] = value;
-        navigator.mozSettings.createLock().set(obj);
+        navigator.mozSettings.createLock().set(obj); // push to global setting
 
-        syncevent.settingValue = value;
+        event.settingValue = value;
       } else {
-        syncevent.settingValue = req.result[setting];
-        console.log('setting ' + setting + ' already found as: ' + req.result[setting]);
+        event.settingValue = req.result[setting]; // if it's already set, don't do anything besides push to syncMozSettings
       }
 
-      console.log('attempting to setup observer');
+      /* Call syncMozSetting whenever the setting is updated by Sunset.app or through WebIDE */
       navigator.mozSettings.addObserver(setting, syncMozSettings);
 
-      syncMozSettings(syncevent);
+      /* Push the fake event (which addObserver would generate) into syncMozSettings */
+      syncMozSettings(event);
     };
   }
 
+  /* init() is the final part of the startup process, it does:
+     1) Sets up callback for when the add-on is uninstalled (Delete Add-on), aka, onuninstall()
+     2) Sets up a callback for when the addon is toggled (Disabled/Enabled),
+     3) Toggles the filter, aka, actually set its hue
+  */
   function init() {
-    console.log('entering init()');
-    /* Set the listeners, launching Sunset whenever it gets enabled */
+
+    /* When you delete the addon, clear minutely timer, then remove the filter */
     navigator.mozApps.mgmt.onuninstall = function() {
       clearTimeout(window._sunsetTimer);
       removeFilter();
     };
+
+    /* When you enable/disable the add-on, call toggle to see if the filter should be added or destroyed */
     navigator.mozApps.mgmt.onenabledstatechange = function(event) {
       toggle(event.application.enabled);
     };
 
-    // navigator.mozSettings.addObserver('screen.sunset.max-alpha', function (event) {
-    //   console.log(event);
-    //   toggle(true);
-    // });
-
-    /* Turn the darned thing on */
+    /* We've finally done it! Turn the filter on! Maybe even grab a beer! */
     toggle(true);
   }
 
+  /* inject() injects the (invisible) screen filter and toggles the z-indicies properly so that the filter
+     doesn't cover up the software buttons */
   function inject() {
     var filter;
-    console.log('entering inject()');
 
     /* Don't allow the filter to be set infinite times; we don't love sunsets *that* much */
     if (document.contains(document.getElementById('sunset'))) {
-      console.log('exiting inject() - sunset already exists');
       return;
     }
 
@@ -94,42 +97,45 @@ var Sunset = (function() {
     /* set all the styles for the filter */
     filter.style.height = '100%';
     filter.style.left = 0;
-    filter.style.pointerEvents = 'none';
+    filter.style.pointerEvents = 'none'; // we actually want to be able to click on the screen
     filter.style.position = 'absolute';
     filter.style.top = 0;
     filter.style.width = '100%';
-    filter.style.zIndex = '10000000';
+    filter.style.zIndex = '25000'; // above #utility-tray, below #software-buttons-fullscreen-layout
+
+    /* adjust the z-index of the taskbar on the bottom, so that sunset doesn't affect it;
+       hopefully this doesn't break something horribly */
+    document.getElementById('software-buttons-fullscreen-layout').style.zIndex = 30000;
 
     /* append the screen filter to the system app */
     document.body.appendChild(filter);
-
-    console.log('successfully injected sunset into system');
   }
 
   /* Set the alpha transparency of the screen filter */
-  function setAlpha(alpha) {
-    console.log('entering setAlpha() with alpha of ' + String(alpha));
-
-    var filter = document.getElementById('sunset');
-    filter.style.backgroundColor = 'rgba(255, 255, 0, ' + String(alpha) + ')';
+  function setAlpha(alpha, taskBarBrightness) {
+    document.getElementById('sunset').style.backgroundColor = 'rgba(255, 255, 0, ' + String(alpha) + ')';
+    document.getElementById('software-home-button').style.filter = 'brightness(' + String(taskBarBrightness) + ')';
   }
 
-  /* Toggle the screen filter on and off */
+  /* toggle() is a bit of a misnomer; it does toggle (setting onoff to true or false), but mostly it is responsible
+     for calculating how yellow the filter should be, as well as how dim the home screen button should be */
   function toggle(onoff) {
-    console.log('entering toggle()');
     var diff = 0;
 
     if (onoff === true) {
-      inject();
+      inject(); // inject just in case the filter got removed with removeFilter()
     } else {
-      removeFilter();
+      removeFilter(); // remove the filter from the screen and get out of here
       return;
     }
 
+    /* We never want more than a single timer to be called to be set on the global scope, else we end up calling
+       toggle() too many times and waste battery */
     if (window._sunsetTimer) {
       clearTimeout(window._sunsetTimer);
     }
 
+    /* Calculate the current time, as well as the sunrise and sunset times, in terms of minutes into the day */
     var curTime = new Date();
     curTime = curTime.getHours() * 60 + curTime.getMinutes();
 
@@ -139,40 +145,46 @@ var Sunset = (function() {
     var sunsetTime = SunsetSettings['screen.sunset.manual-sunset'].split(':');
     sunsetTime = Number(sunsetTime[0]) * 60 + Number(sunsetTime[1]);
 
-    if (curTime < sunriseTime) {
+    /* diff controls how strong the filter is, if it's after sunset or an hour before sunrise, it's set to 1 (100%),
+       otherwise it is set to a percentage of close we are to that time in the proceeding hour.  For example,
+       15 minutes before sunrise = 75% strength (.75), 30 minutes = 50% strength, etc. */
+
+    if (curTime < sunriseTime) { // before sunrise
       diff = sunriseTime - curTime;
     } else if (curTime > (sunsetTime - 60)) { // 60 minutes before sunset
       diff = Math.abs(sunsetTime - 60 - curTime);
     } else {
-      removeFilter();
+      removeFilter(); // if it's between sunrise and 60 minutes before sunset, remove the filter entirely
       return;
     }
 
-    console.log('the sun is down with a difference of ' + String(diff) + ' - let\'s do this!');
     if (diff >= 60) { diff = 1; }
     else if (diff > 0) { diff = diff / 60; }
 
-    /* Calculate the correct alpha setting, based on the time of day */
+    /* Calculate the correct alpha setting for the filter, as well as the proper home button brightness.
+       The home button brightness will vary between 75% (diff == 1), and 100% (diff == 0) */
     if (diff !== 0) {
       var alpha = (SunsetSettings['screen.sunset.max-alpha'] / 100 * diff).toFixed(3);
-      console.log('setting alpha to ' + alpha);
-      setAlpha(alpha);
+      var brightness = (1 - (diff * .25)).toFixed(3); // min: .75, max: 1
+
+      setAlpha(alpha, brightness);
     }
 
+    /* Call toggle every minute so that the filter gets darker and brighter throughout the day */
     window._sunsetTimer = setTimeout(function () {
       toggle(true).bind(this);
     }, 60000);
 
-    console.log('exiting toggle()');
   }
 
+  /* Remove the screen filter (for performance), set the software button panel's zindex back to auto, and
+     set the home button back to its default brightness (100%) */
   function removeFilter() {
-    console.log('entering removeFilter()');
     var filter = document.getElementById('sunset');
     filter.parentNode.removeChild(filter);
-    console.log('exiting removeFilter()');
+    document.getElementById('software-buttons-fullscreen-layout').style.zIndex = 'auto';
+    document.getElementById('software-home-button').style.filter = 'none';
   }
-
 
   return {
     init: init,
@@ -184,15 +196,13 @@ var Sunset = (function() {
 }());
 
 /* Attempt to initialize the application */
-if (document.documentElement) {
-  console.log('enabling sunset inside system on being enabled');
+if (document.documentElement) { // system already exists, ie, enable/disable
   Sunset.setUp();
   Sunset.inject();
   Sunset.init();
 } else {
-  window.addEventListener('DOMContentLoaded', function () {
+  window.addEventListener('DOMContentLoaded', function () { // bootup
     'use strict';
-    console.log('enabling sunset inside system on startup');
     Sunset.setUp();
     Sunset.inject();
     Sunset.init();
