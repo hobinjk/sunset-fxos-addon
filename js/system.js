2e91b0bc95e18bfc9e8b070c8c660ea27efd177d
James Hobin <hobinjk@mit.edu>
2015-11-26 03:06:34 -0500

    Get minimal skeleton running

--- js/system.js
+++ js/system.js
@@ -1,65 +1,27 @@
 /*eslint-env browser */
+console.error('Sunrise system exists');
 
 /* Try not to pollute the global namespace too much */
-var Sunset = (function() {
+var Sunrise = (function() {
   'use strict';
 
   /* a carefully chosen set of times, based on what my gut and eyeballs told me */
-  var DEFAULT_SETTINGS = {
-    'screen.sunset.geolocate': true,
+  var SunriseSettings = {
+    'screen.sunset.geolocate': false,
     'screen.sunset.max-alpha': 17, // your foxfood may vary
     'screen.sunset.manual-sunrise': '07:00',
     'screen.sunset.manual-sunset': '20:00'
   };
-
-  /* Replicate the Sunset settings into SunsetSettings, so we don't have to constantly make async
-     calls to the painful Settings API.  Once an update is made, call toggle so that the screen
-     properly reflects the change */
-  var SunsetSettings = {};
-  function syncMozSettings(event) {
-    SunsetSettings[String(event.settingName)] = event.settingValue;
-    toggle();
-  }
 
   /* Whenever the application is initialized, we need to do some setup to:
     1) Replicate the DEFAULT_SETTINGS to Global Settings, if they don't already exist
     2) Push the proper setting (either default or global into mozSyncSettings())
     3) Set observers that call back to syncMozSettings whenever they're changed */
   function setUp() {
-    for (var setting in DEFAULT_SETTINGS) {
-      setUpGetSetMonitorValues(setting, DEFAULT_SETTINGS[setting]);
-    }
 
     /* Call toggle() whenever the attributes on 'screen' change (particularly locked), to set size to 100% */
     window._sunsetSoftwareButtonMO = new MutationObserver(toggle);
     window._sunsetSoftwareButtonMO.observe(document.getElementById('screen'), { attributes: true });
-  }
-
-  function setUpGetSetMonitorValues(setting, value) {
-    setting = String(setting);
-
-    /* Create a fake event like what createLock().get() returns that we will use when calling syncMozSettings */
-    var event = {};
-    event.settingName = setting;
-
-    var req = navigator.mozSettings.createLock().get(setting); // the Settings API makes this unnecessarily difficult
-    req.onsuccess = function () {
-      if (!(setting in req.result)) { // global pref doesn't exist yet
-        var obj = {};
-        obj[setting] = value;
-        navigator.mozSettings.createLock().set(obj); // push to global setting
-
-        event.settingValue = value;
-      } else {
-        event.settingValue = req.result[setting]; // if it's already set, don't do anything besides push to syncMozSettings
-      }
-
-      /* Call syncMozSetting whenever the setting is updated by Sunset.app or through WebIDE */
-      navigator.mozSettings.addObserver(setting, syncMozSettings);
-
-      /* Push the fake event (which addObserver would generate) into syncMozSettings */
-      syncMozSettings(event);
-    };
   }
 
   /* init() is the final part of the startup process, it does:
@@ -78,7 +40,7 @@ var Sunset = (function() {
     /* When you enable/disable the add-on, call toggle to see if the filter should be added or destroyed */
     navigator.mozApps.mgmt.onenabledstatechange = function(event) {
       var app = event.application;
-      if (app.manifest.name === 'Sunset') {
+      if (app.manifest.name === 'Sunrise') {
         toggle(app.enabled);
       }
     };
@@ -123,8 +85,8 @@ var Sunset = (function() {
 
   /* Set the alpha transparency of the screen filter */
   function setAlpha(alpha, taskBarBrightness) {
-    document.getElementById('sunset').style.backgroundColor = 'rgba(255, 255, 0, ' + String(alpha) + ')';
-    document.getElementById('software-home-button').style.filter = 'brightness(' + String(taskBarBrightness) + ')';
+    document.getElementById('sunset').style.backgroundColor = 'rgba(255, 255, 0, ' + alpha + ')';
+    document.getElementById('software-home-button').style.filter = 'brightness(' + taskBarBrightness + ')';
   }
 
   /* toggle() is a bit of a misnomer; it does toggle (setting onoff to true or false), but mostly it is responsible
@@ -151,12 +113,6 @@ var Sunset = (function() {
       toggle.call(this);
     }, 60000);
 
-    /* Sometimes the Settings API is a bit slow in feeding into SunsetSettings; we'll just return and catch it the
-       next time about */
-    if (!('screen.sunset.manual-sunset' in SunsetSettings)) {
-      return;
-    }
-
     /* to avoid having to mess with the software button's z-index, we calculate its height, and then set the
        screen filter's width/height to 100% minus that size */
     var filter = document.getElementById('sunset');
@@ -178,10 +134,10 @@ var Sunset = (function() {
     var curTime = new Date();
     curTime = curTime.getHours() * 60 + curTime.getMinutes();
 
-    var sunriseTime = SunsetSettings['screen.sunset.manual-sunrise'].split(':');
+    var sunriseTime = SunriseSettings['screen.sunset.manual-sunrise'].split(':');
     sunriseTime = Number(sunriseTime[0]) * 60 + Number(sunriseTime[1]);
 
-    var sunsetTime = SunsetSettings['screen.sunset.manual-sunset'].split(':');
+    var sunsetTime = SunriseSettings['screen.sunset.manual-sunset'].split(':');
     sunsetTime = Number(sunsetTime[0]) * 60 + Number(sunsetTime[1]);
 
     /* diff controls how strong the filter is, if it's after sunset or an hour before sunrise, it's set to 1 (100%),
@@ -203,7 +159,7 @@ var Sunset = (function() {
     /* Calculate the correct alpha setting for the filter, as well as the proper home button brightness.
        The home button brightness will vary between 75% (diff == 1), and 100% (diff == 0) */
     if (diff !== 0) {
-      var alpha = (SunsetSettings['screen.sunset.max-alpha'] / 100 * diff).toFixed(3);
+      var alpha = (SunriseSettings['screen.sunset.max-alpha'] / 100 * diff).toFixed(3);
       var brightness = (1 - (diff * .25)).toFixed(3); // min: .75, max: 1
 
       setAlpha(alpha, brightness);
@@ -231,14 +187,14 @@ var Sunset = (function() {
 
 /* Attempt to initialize the application */
 if (document.documentElement) { // system already exists, ie, enable/disable
-  Sunset.setUp();
-  Sunset.inject();
-  Sunset.init();
+  Sunrise.setUp();
+  Sunrise.inject();
+  Sunrise.init();
 } else {
   window.addEventListener('DOMContentLoaded', function () { // bootup
     'use strict';
-    Sunset.setUp();
-    Sunset.inject();
-    Sunset.init();
+    Sunrise.setUp();
+    Sunrise.inject();
+    Sunrise.init();
   });
 }
